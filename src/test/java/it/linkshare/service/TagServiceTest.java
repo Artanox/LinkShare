e6c7a01fc1893e0d387f7df91decc0249d82c281
Artanox <vincenzoemanuele.carusotto@community.unipa.it>
2022-03-12 16:06:35 +0100

    added @BeforeAll

--- src/test/java/it/linkshare/service/TagServiceTest.java
+++ src/test/java/it/linkshare/service/TagServiceTest.java
@@ -1,6 +1,7 @@
 package it.linkshare.service;
 import it.linkshare.model.Tag;
 import it.linkshare.repository.TagRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,38 +17,40 @@ class TagServiceTest {
 
     @Mock private TagRepository tagRepository;
     @InjectMocks private TagService tagService;
+    static private Tag tag;
+
+    @BeforeAll
+    static void beforeAll(){
+        tag = new Tag();
+    }
 
     @Test
     void getAllTag() {
         when(tagRepository.findAll())
-                .thenReturn(Stream.of(new Tag()).collect(Collectors.toList()));
+                .thenReturn(Stream.of(new Tag(), new Tag(), new Tag()).collect(Collectors.toList()));
         assertEquals(1, tagService.getAllTag().size());
     }
 
     @Test
     void getTagById() {
-        Tag tag = new Tag();
         when(tagRepository.getById(tag.getId())).thenReturn(tag);
         assertEquals(tag, tagService.getTagById(tag.getId()));
     }
 
     @Test
     void addNewTag() {
-        Tag tag = new Tag();
         when(tagRepository.save(tag)).thenReturn(tag);
         assertEquals(tag, tagService.addNewTag(tag));
     }
 
     @Test
     void updateTag() {
-        Tag tag = new Tag();
         when(tagRepository.save(tag)).thenReturn(tag);
         assertEquals(tag, tagService.updateTag(tag));
     }
 
     @Test
     void deleteTag() {
-        Tag tag = new Tag();
         tagService.deleteTag(tag);
         verify(tagRepository, times(1)).delete(tag);
     }
